e5cc480984aba0d4acd6b2a8701ae67b5ab3cfbd
Prajwal <prajwaljagadeesh2001@gmail.com>
2024-12-21 23:42:01 +0530

    postcreation

--- social/src/components/NewPost.js
+++ social/src/components/NewPost.js
@@ -6,6 +6,7 @@ import Camera from "../assets/camera.png";
 import Vector from "../assets/Vector.png";
 import vimg from "../assets/vimg.png";
 import { useNavigate } from 'react-router-dom';
+import AddMore from "../assets/addmore.png";
 
 
 const NewPost = () => {
@@ -38,65 +39,83 @@ const NewPost = () => {
         <textarea className='txt' placeholder=' Whats on your maind?'/>
       </div>
 
-      {!showFileOptions ? (
-        <div className='fold' onClick={()=>setShowFileOptions(true)}>
-        <img src={Folder} alt='folder' className='folder'/>
+      {!selectedFile ? (
+  <>
+    {!showFileOptions ? (
+      <div className='fold' onClick={() => setShowFileOptions(true)}>
+        <img src={Folder} alt='folder' className='folder' />
         <p className='choose'>Choose the files</p>
       </div>
-      ):(
-        <>
+    ) : (
+      <>
         <div className='gallery'>
-        <label htmlFor="photo-input" className="gal-img">
-        <img src={Vector} alt='gallery' className='gal-photo'/>
-        Photos
-        </label>
-        <input
-        type='file'
-        id="photo-input"
-        accept="image/*"
-        style={{ display: 'none' }}
-        onChange={handleFileSelection}
+          <label htmlFor='photo-input' className='gal-img'>
+            <img src={Vector} alt='gallery' className='gal-photo' />
+            Photos
+          </label>
+          <input
+            type='file'
+            id='photo-input'
+            accept='image/*'
+            style={{ display: 'none' }}
+            onChange={handleFileSelection}
+          />
+        </div>
+
+        <div className='vt-cont'>
+          <label htmlFor='video-input' className='gal-vedio'>
+            <img src={vimg} alt='video' className='gal-ved' />
+            Video
+          </label>
+          <input
+            type='file'
+            id='video-input'
+            accept='video/*'
+            style={{ display: 'none' }}
+            onChange={handleFileSelection}
+          />
+        </div>
+      </>
+    )}
+
+    <div className='camera'>
+      <img src={Camera} alt='camera' className='cam' />
+      <p className='camtxt'>Camera</p>
+    </div>
+  </>
+) : (
+  <>
+    {selectedFile.type.startsWith('image/') && (
+      <div className='file-preview'>
+        <img
+          src={URL.createObjectURL(selectedFile)}
+          alt='Selected'
+          className='preview-image'
         />
       </div>
+    )}
+    {selectedFile.type.startsWith('video/') && (
+      <div className='file-preview'>
+        <video controls className='preview-video'>
+          <source src={URL.createObjectURL(selectedFile)} />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    )}
 
-      <div className='vt-cont'>
-      <label htmlFor="photo-input" className="gal-vedio">
-        <img src={vimg} alt='vedio' className='gal-ved'/>
-        Vedio
-        </label>
-        <input
-    type='file'
-    id="video-input"
-    accept="video/*"
-    style={{ display: 'none' }}
-    onChange={handleFileSelection} // Function to handle video file selection
-  />
-      </div>
-        </>
-      )}
-      {selectedFile && (
-        <div className='file-preview'>
-          {selectedFile.type.startsWith('image/') && (
-            <img
-              src={URL.createObjectURL(selectedFile)}
-              alt='Selected'
-              className='preview-image'
-            />
-          )}
-          {selectedFile.type.startsWith('video/') && (
-            <video controls className='preview-video'>
-              <source src={URL.createObjectURL(selectedFile)} />
-              Your browser does not support the video tag.
-            </video>
-          )}
-        </div>
-      )}
+    {/* Add More Section */}
+    <div className='addmore'>
+      <img src={AddMore} alt='addmore' className='addmore-gal' />
+      <p className='addmoretxt'>Add more Photos</p>
       
+    </div>
+    <div className='newDiv'>
+      <p>Surrounded by nature’s beauty, finding peace in every leaf, breeze, and sunset. 🌿🌞 
+     <span>#NatureVibes #OutdoorEscape #EarthLover</span></p>
+    </div>
+  </>
+)}
       
-      <div className='camera'>
-      <img src={Camera} alt='camera' className='cam'/>
-      <p className='camtxt'>Camera</p>
-      </div>
       <div className='new-btn'>
       <button className='cre-btn'>CREATE</button>
       </div>
